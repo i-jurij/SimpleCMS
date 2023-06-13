4dc8ec4dedffaf0fea3547ba5f48afd5529fdbd0
I-Jurij <yjurij@gmail.com>
2023-06-13 17:29:06 +0300

    bugfix in master

---
+++ app/Http/Controllers/Client/SignupController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Client;
+
+use App\Http\Controllers\Controller;
+
+class SignupController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
